8ff5c92ee8bbc129c36def44cc3ae4b8f92b425a
Leonardo Picchi Dancona <lpdancona@gmail.com>
2023-03-06 12:16:38 -0800

    adding function to generate question

--- main.rb
+++ main.rb
@@ -8,3 +8,13 @@ class Player
     @lives = 3
   end
 end
+# method to generate a new math question
+def generate_question
+  num1 = rand(1..20)
+  num2 = rand(1..20)
+  puts "What is #{num1} + #{num2}?"
+  return num1 + num2
+end
+
+player1 = Player.new("Player 1")
+player2 = Player.new("Player 2")
